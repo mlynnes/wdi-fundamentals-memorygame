c7941b181434f1fbebfe380e350597f2b29286df
mlynnes <micaelalsassano@gmail.com>
2017-03-19 20:18:39 -0400

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,27 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped queen");
+var cardId = cards [0, 1, 2, 3];
 
-var cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped king");
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  }
+  else {
+      console.log("Sorry, try again.");
+  };
+};
 
- 
-if (cardOne === cardTwo) {
- 	alert("You found a match!");
- }
- else if (cardOne === cardTwo); {
- 	alert("Sorry, try again.");
- }
+var flipCard = function (cardId) {
+	console.log("User flipped" + cards[cardId]);
+		if (cards[0] === cards[2]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		};
+		cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+	};	
+
+flipCard(0);
+flipCard(2);
+

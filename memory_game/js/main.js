78a73dc9082322b2fcff6cda272fbc637448db08
mlynnes <micaelalsassano@gmail.com>
2017-03-19 22:02:38 -0400

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank : "queen",
+		suit : "hearts",
+		cardImage : "images/queen-of-hearts.png"
+	},
+	{
+		rank : "queen",
+		suit : "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank : "king",
+		suit : "hearts",
+		cardImage : "images/king-of-hearts.png"
+	},
+	{
+		rank : "king",
+		suit : "diamonds",
+		cardImage : "images/king-of-diamonds.png"
+	}
+];
+
 var cardsInPlay = [];
 var cardId = cards [0, 1, 2, 3];
 
@@ -12,16 +34,20 @@ var checkForMatch = function () {
 };
 
 var flipCard = function (cardId) {
-	console.log("User flipped" + cards[cardId]);
+	console.log("User flipped" + cards[cardId].rank);
 		if (cards[0] === cards[2]) {
 			alert("You found a match!");
 		} else {
 			alert("Sorry, try again.");
 		};
-		cardsInPlay.push(cards[cardId]);
+			
 	checkForMatch();
 	};	
 
 flipCard(0);
+console.log("images/queen-of-diamonds.png");
+console.log("hearts");
 flipCard(2);
+console.log("images/king-of-hearts.png");
+console.log("hearts")
 
